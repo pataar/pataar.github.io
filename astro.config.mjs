d2dcb9654376fea996f8d6525ad94eef0095740f
pataar <git@pataar.nl>
2023-09-01 10:58:40 +0200

    upgrade to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true
-  },
   site: 'https://pieterwillekens.nl',
-  build: {
-    inlineStylesheets: 'auto'
-  },
   integrations: [tailwind({
     applyBaseStyles: false
   }), sitemap(), react()]
