8bf18c0cccc0907d112e0503fdf6ccf7b7bbf558
pataar <pietering1@gmail.com>
2025-02-11 09:15:31 +0100

    use latest stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pieterwillekens.nl',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), react()]
+	site: "https://pieterwillekens.nl",
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		react(),
+	],
+	vite: { plugins: [tailwindcss()] },
 });
